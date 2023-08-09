56ce743329056bd89cb22bb7599006db23f6e2d5
Daniel Blindu <daniel.blindu@runviably.com>
2023-08-09 18:36:13 +0200

    update for hanging urls

--- cmd/url2wordlist/main.go
+++ cmd/url2wordlist/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,76 +10,49 @@ import (
 	"sort"
 	"strings"
 	"sync"
+	"time"
 )
 
-func fetchWordsFromURL(url string, wg *sync.WaitGroup, resultChan chan<- []string) {
+func fetchWordsFromURL(urlStr string, wg *sync.WaitGroup, resultChan chan<- []string) {
 	defer wg.Done()
 
-	response, err := http.Get(url)
+	// Create a context with a timeout of 5 seconds for the URL operation
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", urlStr, nil)
 	if err != nil {
-		fmt.Printf("Error fetching URL %s: %s\n", url, err)
 		resultChan <- nil
 		return
 	}
-	defer response.Body.Close()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		resultChan <- nil
+		return
+	}
+	defer resp.Body.Close()
 
 	var words []string
-	scanner := bufio.NewScanner(response.Body)
+	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
-		line := scanner.Text()
-		// Use a regular expression to find valid words (alphanumeric, underscore, hyphen)
-		validWordRegex := regexp.MustCompile(`[\w-]+`)
-		matches := validWordRegex.FindAllString(line, -1)
-		words = append(words, matches...)
+		words = append(words, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading response from URL %s: %s\n", url, err)
 		resultChan <- nil
 		return
 	}
 
-	resultChan <- words
-}
+	validWordRegex := regexp.MustCompile(`[\w-]+`)
+	filteredWords := make([]string, 0)
 
-func mergeWords(wordLists [][]string) []string {
-	merged := make([]string, 0)
-	for _, wordList := range wordLists {
-		merged = append(merged, wordList...)
-	}
-	return merged
-}
-
-func countWords(words []string) map[string]int {
-	wordCount := make(map[string]int)
-	for _, word := range words {
-		word = strings.ToLower(word)
-		wordCount[word]++
-	}
-	return wordCount
-}
-
-func sortByOccurrence(wordCount map[string]int) []string {
-	type wordFrequency struct {
-		word      string
-		occurrence int
+	for _, line := range words {
+		matches := validWordRegex.FindAllString(line, -1)
+		filteredWords = append(filteredWords, matches...)
 	}
 
-	var wordFreqList []wordFrequency
-	for word, count := range wordCount {
-		wordFreqList = append(wordFreqList, wordFrequency{word, count})
-	}
-
-	sort.Slice(wordFreqList, func(i, j int) bool {
-		return wordFreqList[i].occurrence > wordFreqList[j].occurrence
-	})
-
-	var sortedWords []string
-	for _, wf := range wordFreqList {
-		sortedWords = append(sortedWords, wf.word)
-	}
-
-	return sortedWords
+	resultChan <- filteredWords
 }
 
 func main() {
@@ -91,9 +65,10 @@ func main() {
 	var wg sync.WaitGroup
 	resultChan := make(chan []string)
 
-	for _, url := range urls {
+	for _, urlStr := range urls {
 		wg.Add(1)
-		go fetchWordsFromURL(url, &wg, resultChan)
+
+		go fetchWordsFromURL(urlStr, &wg, resultChan)
 	}
 
 	go func() {
@@ -101,19 +76,36 @@ func main() {
 		close(resultChan)
 	}()
 
-	var allWords [][]string
+	var allWords []string
 	for words := range resultChan {
 		if words != nil {
-			allWords = append(allWords, words)
+			allWords = append(allWords, words...)
 		}
 	}
 
-	mergedWords := mergeWords(allWords)
-	wordCount := countWords(mergedWords)
-	sortedWords := sortByOccurrence(wordCount)
+	wordCount := make(map[string]int)
+	for _, word := range allWords {
+		word = strings.ToLower(word)
+		wordCount[word]++
+	}
 
+	var sortedWords []string
+	for word := range wordCount {
+		sortedWords = append(sortedWords, word)
+	}
+
+	// Sort words by occurrence
+	sortWordsByOccurrence(sortedWords, wordCount)
+
+	// Print the sorted words
 	for _, word := range sortedWords {
 		fmt.Println(word)
 	}
+}
+
+func sortWordsByOccurrence(words []string, wordCount map[string]int) {
+	sort.Slice(words, func(i, j int) bool {
+		return wordCount[words[i]] > wordCount[words[j]]
+	})
 }
 
